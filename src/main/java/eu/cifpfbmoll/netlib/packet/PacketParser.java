767502ed47d702d40d5b8ce4784952e8aa3bbec8
Snebot <snebot@cifpfbmoll.eu>
2021-05-08 11:42:11 +0200

    added PacketAttribute string support

--- src/main/java/eu/cifpfbmoll/netlib/packet/PacketParser.java
+++ src/main/java/eu/cifpfbmoll/netlib/packet/PacketParser.java
@@ -15,6 +15,14 @@ import java.util.Map;
  */
 public class PacketParser {
     /**
+     * Dynamically get object fields' size.
+     */
+    @FunctionalInterface
+    private interface SizeHandler {
+        int handle(Object object, Field field) throws IllegalAccessException;
+    }
+
+    /**
      * Serialize/Deserialize object's fields to a byte array using a ByteBuffer.
      */
     @FunctionalInterface
@@ -29,19 +37,19 @@ public class PacketParser {
      * a specific Object type using TypeHandler.</p>
      */
     private static final class TypeInfo {
-        public final int size;
+        public final SizeHandler size;
         public final TypeHandler serializer;
         public final TypeHandler deserializer;
 
-        public TypeInfo(int size, TypeHandler serializer, TypeHandler deserializer) {
+        public TypeInfo(SizeHandler size, TypeHandler serializer, TypeHandler deserializer) {
             this.size = size;
             this.serializer = serializer;
             this.deserializer = deserializer;
         }
     }
 
-    private final Map<Class<?>, TypeInfo> types = new HashMap<>();
     private static final PacketParser instance = new PacketParser();
+    private final Map<Class<?>, TypeInfo> types = new HashMap<>();
 
     /**
      * Get PacketParser's instance.
@@ -53,48 +61,80 @@ public class PacketParser {
     }
 
     public PacketParser() {
-        this.types.put(Byte.TYPE, new TypeInfo(1,
+        this.types.put(Byte.TYPE, new TypeInfo(
+                (object, field) -> 1,
                 (object, field, bb) -> bb.put(field.getByte(object)),
                 (object, field, bb) -> field.setByte(object, bb.get())));
-        this.types.put(Byte.class, new TypeInfo(1,
+        this.types.put(Byte.class, new TypeInfo(
+                (object, field) -> 1,
                 (object, field, bb) -> bb.put((byte) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.get())));
-        this.types.put(Short.TYPE, new TypeInfo(2,
+        this.types.put(Short.TYPE, new TypeInfo(
+                (object, field) -> 2,
                 (object, field, bb) -> bb.putShort(field.getShort(object)),
                 (object, field, bb) -> field.set(object, bb.getShort())));
-        this.types.put(Short.class, new TypeInfo(2,
+        this.types.put(Short.class, new TypeInfo(
+                (object, field) -> 2,
                 (object, field, bb) -> bb.putShort((short) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getShort())));
-        this.types.put(Integer.TYPE, new TypeInfo(4,
+        this.types.put(Integer.TYPE, new TypeInfo(
+                (object, field) -> 4,
                 (object, field, bb) -> bb.putInt(field.getInt(object)),
                 (object, field, bb) -> field.set(object, bb.getInt())));
-        this.types.put(Integer.class, new TypeInfo(4,
+        this.types.put(Integer.class, new TypeInfo(
+                (object, field) -> 4,
                 (object, field, bb) -> bb.putInt((int) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getInt())));
-        this.types.put(Long.TYPE, new TypeInfo(8,
+        this.types.put(Long.TYPE, new TypeInfo(
+                (object, field) -> 8,
                 (object, field, bb) -> bb.putLong(field.getLong(object)),
                 (object, field, bb) -> field.set(object, bb.getLong())));
-        this.types.put(Long.class, new TypeInfo(8,
+        this.types.put(Long.class, new TypeInfo(
+                (object, field) -> 8,
                 (object, field, bb) -> bb.putLong((long) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getLong())));
-        this.types.put(Float.TYPE, new TypeInfo(4,
+        this.types.put(Float.TYPE, new TypeInfo(
+                (object, field) -> 4,
                 (object, field, bb) -> bb.putFloat(field.getFloat(object)),
                 (object, field, bb) -> field.set(object, bb.getFloat())));
-        this.types.put(Float.class, new TypeInfo(4,
+        this.types.put(Float.class, new TypeInfo(
+                (object, field) -> 4,
                 (object, field, bb) -> bb.putFloat((float) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getFloat())));
-        this.types.put(Double.TYPE, new TypeInfo(8,
+        this.types.put(Double.TYPE, new TypeInfo(
+                (object, field) -> 8,
                 (object, field, bb) -> bb.putDouble(field.getDouble(object)),
                 (object, field, bb) -> field.set(object, bb.getDouble())));
-        this.types.put(Double.class, new TypeInfo(8,
+        this.types.put(Double.class, new TypeInfo(
+                (object, field) -> 8,
                 (object, field, bb) -> bb.putDouble((double) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getDouble())));
-        this.types.put(Character.TYPE, new TypeInfo(2,
+        this.types.put(Character.TYPE, new TypeInfo(
+                (object, field) -> 2,
                 (object, field, bb) -> bb.putChar(field.getChar(object)),
                 (object, field, bb) -> field.set(object, bb.getChar())));
-        this.types.put(Character.class, new TypeInfo(2,
+        this.types.put(Character.class, new TypeInfo(
+                (object, field) -> 2,
                 (object, field, bb) -> bb.putChar((char) field.get(object)),
                 (object, field, bb) -> field.set(object, bb.getChar())));
+        this.types.put(String.class, new TypeInfo(
+                (object, field) -> {
+                    String str = (String) field.get(object);
+                    return str.length() + 1;
+                },
+                (object, field, bb) -> {
+                    String str = (String) field.get(object);
+                    byte[] bytes = str.getBytes(Packet.CHARSET_ENCODING);
+                    bb.put((byte) bytes.length);
+                    for (byte b : bytes)
+                        bb.put(b);
+                }, (object, field, bb) -> {
+            byte size = bb.get();
+            byte[] bytes = new byte[size];
+            for (int i = 0; i < size; i++)
+                bytes[i] = bb.get();
+            field.set(object, new String(bytes, Packet.CHARSET_ENCODING));
+        }));
     }
 
     /**
@@ -161,10 +201,12 @@ public class PacketParser {
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             if (field.isAnnotationPresent(PacketAttribute.class)) {
-                size += getTypeInfo(field.getType()).size;
+                TypeInfo typeInfo = getTypeInfo(field.getType());
+                size += typeInfo.size.handle(object, field);
                 fields.add(field);
             }
         }
+        System.out.println("allocating a size of: " + size);
         ByteBuffer bb = ByteBuffer.allocate(size);
         for (Field field : fields)
             getTypeInfo(field.getType()).serializer.handle(object, field, bb);
