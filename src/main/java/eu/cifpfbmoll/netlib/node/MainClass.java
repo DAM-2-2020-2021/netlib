10e6ffbb91e698350bd48e074e301b3d0d533657
Jose Miguel Rivas Mendez <jrivas@cifpfbmoll.eu>
2021-05-23 14:02:10 +0200

    Adding Main class

---
+++ src/main/java/eu/cifpfbmoll/netlib/node/MainClass.java
@@ -0,0 +1,8 @@
+package eu.cifpfbmoll.netlib.node;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        NodeManager nodeManager=new NodeManager(1,"192.168.1.104");
+    }
+}
