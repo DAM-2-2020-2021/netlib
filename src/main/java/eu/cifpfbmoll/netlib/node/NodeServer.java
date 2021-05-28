716aa2f8eca695b5fc185f806dfdee186bc833cd
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2021-05-28 13:32:48 +0200

    fixed NodeServer new connection bug

--- src/main/java/eu/cifpfbmoll/netlib/node/NodeServer.java
+++ src/main/java/eu/cifpfbmoll/netlib/node/NodeServer.java
@@ -4,8 +4,6 @@ import eu.cifpfbmoll.netlib.util.Threaded;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 
@@ -44,23 +42,12 @@ public class NodeServer extends Threaded {
         while (this.run) {
             try {
                 NodeSocket nodeSocket = new NodeSocket(this.socket.accept());
-                log.info("Creating connection with: " + nodeSocket.getIp());
-                if (!this.manager.nodeInHash(nodeSocket.getIp())) {
-                    log.info(String.format("Identifying connection with ip: %s", nodeSocket.getIp()));
-                    //new NodeIdentification(nodeSocket, this.manager);
-                    DataInputStream inputStream = new DataInputStream(nodeSocket.getSocket().getInputStream());
-                    String message = inputStream.readUTF();
-                    if (message.equals("I am Damn player")) {
-                        log.info("Hello message received from: " + nodeSocket.getIp());
-                        DataOutputStream outputStream = new DataOutputStream(nodeSocket.getSocket().getOutputStream());
-                        outputStream.writeUTF("Welcome");
-                        outputStream.flush();
-                    }
-                } /*else {
-                    NodeConnection nodeConnection = new NodeConnection(new Node(NodeManager.counter++, nodeSocket.getIp()), nodeSocket, this.manager);
-                    this.manager.addNewConnection(nodeConnection);
-                    log.info(String.format("New NodeConnection added! %s"), nodeSocket.getIp());
-                }*/
+                if (this.manager.nodeInHash(nodeSocket.getIp())) {
+                    Integer nodeID = this.manager.getNodeIdByIP(nodeSocket.getIp());
+                    NodeConnection nodeConnection = new NodeConnection(new Node(nodeID, nodeSocket.getIp()), nodeSocket, this.manager);
+                    this.manager.addNodeConnection(nodeConnection);
+                    log.info(String.format("New NodeConnection added! %s", nodeSocket.getIp()));
+                }
             } catch (Exception e) {
                 log.error("Error in NodeServer run", e);
             }
